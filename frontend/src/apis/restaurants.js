ffba37606bd47ec818b42efe725ad372fade3882
Kazuya-Sakashita <kz0508@gmail.com>
2021-07-10 21:33:14 +0900

    5-6 レストラン一覧ページ

--- frontend/src/apis/restaurants.js
+++ frontend/src/apis/restaurants.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { restaurantsIndex } from '../urls/index'
+/*URL文字列をimportして使用する*/
 
 export const fetchRestaurants =() => {
   return axios.get(restaurantsIndex)
@@ -8,3 +9,10 @@ export const fetchRestaurants =() => {
   })
   .catch((e) => console.error(e))
 }
+
+/*axiosを使う側でnew Promiseなどしなくても、非同期処理を実装することができる.
+axios.get().then().catch()が可能*/
+
+/*axios.getした結果成功した場合はthen(...)にいき、
+失敗・例外が帰ってきた場合はcatch(...)
+成功した場合は、resという名前で取得して、res.dataでレスポンスの中身をreturn*/
